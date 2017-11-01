da5745d859732dc7eece2864359bef020415adec
Martina <marco@itba.edu.ar>
2017-11-01 15:22:48 -0300

    pcb

--- Kernel/include/PCBTableADT.h
+++ Kernel/include/PCBTableADT.h
@@ -6,14 +6,15 @@
  #define READY 2;
  #define TRUE 1;
  #define FALSE 0;
- 
+ #define NULL 0;
+
+ typedef struct pcbCDT;
+ typedef struct pcbCDT * pcbADT;
 
  static int idCount = 0;
  static pcbADT pcbTable;//= direccion donde quieran ponerlo
  static pcbADT last = NULL;
 
- typedef struct pcbCDT;
- typedef struct pcbCDT * pcbADT;
  void addPCB(int privilege);
  void removePCB(int id);
  void changeState(int id, int state);
