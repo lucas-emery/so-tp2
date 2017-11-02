5cbf0a04562bc9f07551b59f7afd37a940f4113b
Martina <marco@itba.edu.ar>
2017-11-02 09:46:07 -0300

    fix

--- Kernel/include/PCBTableADT.h
+++ Kernel/include/PCBTableADT.h
@@ -1,26 +1,25 @@
 #ifndef PCBTABLEADT_H
 #define PCBTABLEADT_H
 
- #define RUNNING 0;
- #define BLOCKED 1;
- #define READY 2;
- #define TRUE 1;
- #define FALSE 0;
- #define NULL 0;
+#define RUNNING 0
+#define BLOCKED 1
+#define READY 2
+#define TRUE 1
+#define FALSE 0
+#define NULL 0
+#define MAX_CHILDREN 10
+#define MAX_SIZE 100
 
+typedef struct pcbCDT * pcbADT;
 
- typedef struct pcbCDT * pcbADT;
+static int idCount = 0;
+static pcbADT pcbTable[MAX_SIZE];//= direccion donde quieran ponerlo
+static int tableSize = 0;
 
- static int MAX_CHILDREN = 10;
- static int MAX_SIZE = 100;
- static int idCount = 0;
- static pcbADT pcbTable[MAX_SIZE];//= direccion donde quieran ponerlo
- static int tableSize = 0;
-
- int addPCB(int privilege);
- void removePCB(int id);
- void changeState(int id, int state);
- void addChild(int fatherId);
- void createChild(pcbADT father);
+int addPCB(int privilege);
+void removePCB(int id);
+void changeState(int id, int state);
+void addChild(int fatherId);
+void createChild(pcbADT father);
  
- #endif PCBTABLEADT_H
+#endif
