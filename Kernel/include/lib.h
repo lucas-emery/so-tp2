9d08e5e6ae4954494600ca0014d99fe3b21cbffb
andresdigio <adigiovannimartinez@itba.edu.ar>
2017-11-06 23:09:14 -0300

    Otro merge en mi branch para recuperar el stash que hice
    No se que estoy haciendo

--- Kernel/include/lib.h
+++ Kernel/include/lib.h
@@ -10,6 +10,7 @@ uint32_t uintToBase(uint64_t value, char * buffer, uint32_t base);
 int strcmp(const char *str1, const char *str2);
 size_t strlen(const char *str);
 
+void dispatch(uint64_t,uint64_t);
 char *cpuVendor(char *result);
 void writePort(uint8_t port, uint8_t value);
 int readPort(uint8_t port);
