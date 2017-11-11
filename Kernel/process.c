95bffd441961ee406f586a281d345ab40e680a02
DiegoOrlando17 <diegoorlando.1997@gmail.com>
2017-11-10 21:30:40 -0300

    cambiando de compu

--- Kernel/process.c
+++ Kernel/process.c
@@ -1,8 +1,21 @@
 #include <process.h>
 
-int createProcess(/*int moduleIndex, int argc, char *argv[]*/){
-	/*int newProcessID = addPCB(3); // default privilege 3
+int createProcess(char * moduleName, int argc, char *argv[]){
+
+	int moduleIndex = 0;
+	for(int i=0; moduleNames[i] != 0 or !found; i++){
+		if(strcmp(moduleName, moduleNames[i]) == 0)
+			found = TRUE;
+	}
+	if(found)
+		moduleIndex = i-1;
+	else
+		return -1;
+
+	int newProcessID = addPCB(3); // default privilege 3
+
 	tcbADT newProcessThread = createFirstTCB(3, moduleIndex, argc, argv); // default privilege 3
 	addTCB(newProcessThread);
-	return newProcessID;*/
+
+	return newProcessID;
 }
