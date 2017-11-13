530c000259ed716b820b906f14b4d45c755c04d5
Martina Arco <marco@itba.edu.ar>
2017-11-12 21:25:36 -0300

    Fixed producer consumer

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -7,8 +7,8 @@
 
 //char * v = (char*)0xB8000 + 79 * 2;
 
-int haceMensaje(int messageID);
-int leeMensaje(int messageID);
+int producer(int messageID);
+int consumer(int messageID);
 
 int main(int argc, char *argv[]) {
 	//while(1){
@@ -16,27 +16,22 @@ int main(int argc, char *argv[]) {
 	//}
 	int messageID = message_init("Diego", 5);
 	//printf("%d\n", messageID);
-	pthread_create(haceMensaje, messageID);
-	pthread_create(leeMensaje, messageID);
+	pthread_create(producer, messageID);
+	pthread_create(consumer, messageID);
 	while(1);
 }
 
-int haceMensaje(int messageID){
+int producer(int messageID){
 	while(1){
 		message_write(messageID, "capo");
-		char* buffer = malloc(100);
-		buffer = message_read(messageID, buffer);
-		printf("%s\n", buffer);
+		//printf("Produce: capo\n");
 	}
 }
 
-int leeMensaje(int messageID){
+int consumer(int messageID){
 	while(1){
 		char* buffer = malloc(100);
 		message_read(messageID, buffer);
-		if(buffer[0] != 0)
-			printf("%s\n", buffer);
-		else
-			printf("No leyo\n");
+		printf("Consume: %s\n", buffer);
 	}
 }
