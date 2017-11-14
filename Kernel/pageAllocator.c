e25ba9e496bf7150ecfbb0101984cff80a303f4e
lucas <lucas.emery@hotmail.com>
2017-11-14 14:35:21 -0300

    Remove paging mirror images

--- Kernel/pageAllocator.c
+++ Kernel/pageAllocator.c
@@ -12,15 +12,24 @@ static uint64_t size = AVOID_BSS;
 static uint64_t firstCandidate = AVOID_BSS;
 
 void initPageAllocator() {
+  uint64_t i;
   uint32_t ram = *((uint32_t*)SYSTEM_RAM_ADDRESS);
+
   size = ram / (PAGESIZE / MB);
   pages = malloc(size * sizeof(uint8_t));
   firstCandidate = 0;
-  for(uint64_t i = 0; i < size; i++) {
+
+  for(i = 0; i < size; i++) {
     pages[i] = FREE;
   }
   pages[KERNEL/PAGESIZE] = USED;
   pages[KERNEL_HEAP/PAGESIZE] = USED;
+
+  for(i = 0; i < MAPPEDMEMORY / PAGESIZE; i++) {
+    if(i != KERNEL/PAGESIZE && i != KERNEL_HEAP/PAGESIZE) {
+      changePDEPresent(i, NOT_PRESENT);
+    }
+  }
 }
 
 
